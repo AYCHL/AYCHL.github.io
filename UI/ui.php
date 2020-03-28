a89ed12029a837723a9ccc457db7098db378815e
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-03-28 15:13:32 +0700

    Update ui.php

--- UI/ui.php
+++ UI/ui.php
@@ -6,7 +6,7 @@
 *
 *  AYCHL is proxy interception for lan and Wi-Fi. It is a free software: you can redistribute it and/or modify
 *  it under the terms of the GNU Affero General Public License as published by
-*  the Free Software Foundation, either version 3 of the License, or
+*  the AYCH inc, either version 3 of the License, or
 *  (at your option) any later version.
 *
 *  AYCHL is distributed in the hope that it will be useful,
